c12f21b69e4e96f5a12212ccb173999d516b752c
John Peterson <jlpeterson829@gmail.com>
2015-02-04 12:25:58 -0800

    Pass first three tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,21 @@
 var Stack = function(){
   var someInstance = {};
-
+  var count = 0; 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    count += 1;
+
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
+    return count;
   };
-
+ 
   return someInstance;
 };
