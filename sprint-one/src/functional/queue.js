59c7d947b26fee0890d5ba43a2e3159bed55cd3c
John Peterson <jlpeterson829@gmail.com>
2015-02-04 17:05:48 -0800

    Pass functional patter queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+  var first = 1;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    count += 1;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function(){
+    var remove = storage[first];
+    delete storage[first];
+    first += 1;
+    return remove;
   };
 
   someInstance.size = function(){
+    if(count-(first-1) >= 0){
+      return count-(first-1);
+    } else {
+      return 0;
+    }
   };
 
   return someInstance;
