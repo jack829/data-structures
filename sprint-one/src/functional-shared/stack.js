5baac44d8386a957547c18b8d3c04204fed2580e
John Peterson <jlpeterson829@gmail.com>
2015-02-05 11:41:55 -0800

    Add functional shared stack, passes tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+
+  var someInstance = {
+    storage: {},
+    count: 0
+  };
+
+  _.extend(someInstance,stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.count += 1;
+    this.storage[this.count] = value;
+  },
+  pop: function(){
+    var remove = this.storage[this.count];
+    delete this.storage[this.count];
+    if(this.count){
+      this.count -= 1;
+    }
+    return remove;
+  },
+  size: function(){
+    return this.count;
+  }
+};
 
 
