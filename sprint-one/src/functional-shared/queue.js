f5849c5e0d54dcd3616a6ae6f3233ba203d959fd
John Peterson <jlpeterson829@gmail.com>
2015-02-04 17:31:05 -0800

    Add first full attempted code

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,34 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    count: 0,
+    first: 1,
+    storage: {}
+  };
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.count += 1;
+    this.storage[count] = value;
+  },
+  dequeue: function(){
+    var remove = this.storage[first];
+    delete this.storage[first];
+    this.first += 1;
+    return remove;
+  },
+  size: function(){
+    if(this.count-(this.first-1) >= 0){
+      return this.count-(this.first-1);
+    } else {
+      return 0;
+    }
+  }
+};
 
 
 
