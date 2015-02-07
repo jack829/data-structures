a6b715a16fc51441019f8f0c6aba2085c2dd4e7c
John Peterson <jlpeterson829@gmail.com>
2015-02-06 19:06:03 -0800

    Update binary search tree through insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,36 @@
 var BinarySearchTree = function(value){
+  var someInstance = Object.create(binaryPrototype);
+  someInstance.left = null;
+  someInstance.right = null;
+  someInstance.value = value;
+  return someInstance;
 };
 
+var binaryPrototype = {};
+
+binaryPrototype.insert = function(value){
+  if (value < this.value) {
+    if (this.left === null){
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    } 
+  } else {
+      if (this.right === null){
+        this.right = BinarySearchTree(value);
+    } else {
+        this.right.insert(value);
+    } 
+  }
+};
+
+binaryPrototype.contains = function(target){
+
+};
+
+binaryPrototype.depthFirstLog = function(func){
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
